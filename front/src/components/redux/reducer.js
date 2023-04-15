a96541123cbc23828fba68a1c0a73b48dc1b689b
rrojastransformait <rrojas@transformait.com.mx>
2023-04-14 23:23:13 -0600

    Working on state managment with redux

--- front/src/components/redux/reducer.js
+++ front/src/components/redux/reducer.js
@@ -1,9 +1,25 @@
+import { ADD_FAV, REMOVE_FAV } from "./actions";
+
 const initialState = {
     myFavorites: []
 };
 
-const rootReducer = (state = initialState, { type, payload }) => {
+export const rootReducer = (state = initialState, action) => { //action : type, payload
+    switch (action.type) {
+        case ADD_FAV:
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, action.payload]
+            }   
+        case REMOVE_FAV:
+            return {
+                ...state,
+                myFavorites: state.myFavorites.filter(favorite => favorite.id !== action.payload.id)
+            } 
+        default:
+            return state;
+    }
 
 };
 
-export default rootReducer;
+//export default rootReducer;
--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import store from './components/redux/store'
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
